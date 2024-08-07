eef9679df6787cc2e853fb30395c1384d1f879f8
晨晔 孙 <2440638601@qq.com>
2024-08-07 12:55:28 +0800

    增加跨域设置

--- Program.cs
+++ Program.cs
@@ -13,6 +13,19 @@ builder.Services.AddDbContext<IndustrialControlAlarmSystemContext>(options =>
 #endregion
 // Add services to the container.
 
+//¿çÓòÅäÖÃ
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -39,6 +52,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseAuthorization();
 
